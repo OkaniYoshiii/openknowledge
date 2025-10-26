42f4ddbd15523ab456da6e78ec8584d969048f11
OkaniYoshiii <dragut31620@gmail.com>
2025-10-26 08:59:34 +0100

    feat: connection à la base de données

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal(err)
+	}
+
+	user := os.Getenv("DATABASE_USER")
+	password := os.Getenv("DATABASE_PASSWORD")
+	address := os.Getenv("DATABASE_ADDRESS")
+	protocol := os.Getenv("DATABASE_PROTOCOL")
+	name := os.Getenv("DATABASE_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@%s(%s)/%s?charset=utf8", user, password, protocol, address, name)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.SetConnMaxLifetime(4 * time.Minute)
+	db.SetMaxOpenConns(5)
+	db.SetMaxIdleConns(5)
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
 }
